94b2da80a5c470dd2e3fe4467551109cc879e861
Tom There <tom.schreiner@gmx.de>
2024-10-15 09:01:59 +0200

    played around with nvim config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,4 +2,13 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 999,
+  config = function()
+    require("catppuccin").setup{
+      flavour = "mocha",
+      term_colors = true,
+      transparent_background = false,
+      no_italic = false,
+      no_bold = false,
+    }
+  end,
 }
