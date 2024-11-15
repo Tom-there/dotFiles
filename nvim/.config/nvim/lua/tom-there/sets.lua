1f7aecbdffa92cd7673a8100cbfbd4cfa678c84a
Tom There <tom.schreiner@gmx.de>
2024-11-15 09:06:28 +0100

    updated nvim settings

--- nvim/.config/nvim/lua/tom-there/sets.lua
+++ nvim/.config/nvim/lua/tom-there/sets.lua
@@ -1,31 +1,56 @@
-vim.opt.nu = true
+-- Left column settings
+vim.opt.number = true
+vim.opt.numberwidth = 2
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
+-- Tab spacing
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 
-vim.opt.wrap = false
-
+-- General
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 0 -- show concealed characters
+vim.opt.fileencoding = "utf-8"
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.nu = true
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.termguicolors = true
+vim.g.have_nerd_font = true
+vim.opt.isfname:append("@-@")
+
+-- optics
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "75"
+
+-- Timings
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 666
+
+-- splitting behavior
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- undos
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+
+-- searching
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "75"
-
+-- leader key
 vim.g.mapleader = " "
