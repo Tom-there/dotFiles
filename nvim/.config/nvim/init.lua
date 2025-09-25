e31a8d7b267f10682aaf18d129289ae1b77582ea
Tom There <tom.schreiner@gmx.de>
2025-09-25 09:18:40 +0200

    moved plugins to own directory and added a few more

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,24 +1,14 @@
 require('core')
 require('colors')
 
+require('plugins.fugitive')
+
 require('lsp')
-require('neotree')
-require('telescope')
+require('plugins.neotree')
+require('plugins.telescope')
 
-require('autopairs')
-
-vim.pack.add({
-	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
-	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-	{ src = "https://github.com/folke/todo-comments.nvim" },
-	{ src = "https://github.com/mbbill/undotree" },
-	--dependencies
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/MunifTanjim/nui.nvim" },
-	{ src = "https://github.com/3rd/image.nvim" },
-})
-
-
-
+require('plugins.autopairs')
+require('plugins.blankline')
+require('plugins.lualine')
+require('plugins.todo_comments')
 
---
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+	{ src = "https://github.com/mbbill/undotree" },
+})
+
+
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
